690ddc076776a7c994773db37d4bd22a16675353
JB Magtoto <82630490+Dev-JBM@users.noreply.github.com>
2026-08-13 19:51:18 +0800

    Added a next round logic and game restart logic. Added a Next Round button with its function for the game continuation. Added a Restart button with its function for restarting round and score numbers.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-// Variable container for buttons
+// Variable container for buttons and button containers
+const optionButtonContainer = document.getElementById("optionButtonContainer");
 const rockBtn = document.getElementById("rockBtn");
 const paperBtn = document.getElementById("paperBtn");
 const scissorBtn = document.getElementById("scissorBtn");
+const roundButtonContainer = document.getElementById("roundButtonContainer");
+const continueBtn = document.getElementById("continueBtn");
+const restartBtn = document.getElementById("restartBtn");
 
 // Variable containter for round number, status, score numbers, and images
 const roundNum = document.getElementById("roundNum");
@@ -13,7 +17,7 @@ const computerImg = document.getElementById("computerImg");
 
 // Global Variables
 let HumanChoice;
-let roundNumValue = 0;
+let roundNumValue = 1;
 let humanScoreValue = 0;
 let computerScoreValue = 0;
 
@@ -69,8 +73,45 @@ function playRound(humanChoice, computerChoice) {
     computerScore.textContent = `${computerScoreValue}`;
   }
 
-  roundNumValue++
+  roundButtonContainer.style.display = "block";
+  optionButtonContainer.style.display = "none";
+
+}
+
+// Function for proceeding to the next round
+function roundContinue() {
+  roundButtonContainer.style.display = "none";
+  optionButtonContainer.style.display = "block";
+
+  roundNumValue++;
   roundNum.textContent = `${roundNumValue}`;
+
+  status.style.backgroundColor = "#6b7785";
+  status.textContent = "Start!"; 
+
+  humanImg.src = `./images/rock.png`;
+  computerImg.src = `./images/rock.png`;
+}
+
+// Function for restarting the game
+function roundRestart() {
+  roundButtonContainer.style.display = "none";
+  optionButtonContainer.style.display = "block";
+
+  roundNumValue = 1;
+  roundNum.textContent = `${roundNumValue}`;
+
+  status.style.backgroundColor = "#6b7785";
+  status.textContent = "Start!"; 
+
+  humanScoreValue = 0;
+  humanScore.textContent = `${humanScoreValue}`;
+
+  computerScoreValue = 0;
+  computerScore.textContent = `${computerScoreValue}`;
+
+  humanImg.src = `./images/rock.png`;
+  computerImg.src = `./images/rock.png`;
 }
 
 
@@ -93,5 +134,10 @@ scissorBtn.addEventListener("click", () => {
   getComputerChoice();
   playRound(getHumanChoice(), getComputerChoice());
 });
-
+continueBtn.addEventListener("click", () => {
+  roundContinue()
+});
+restartBtn.addEventListener("click", () => {
+  roundRestart();
+});
 
